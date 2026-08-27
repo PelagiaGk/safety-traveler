f779d8bfac046f3ceb06e813e16a1ab9414e6478
Pelagia Gkalimani <pelagiagkalimani@gmail.com>
2026-08-27 20:22:20 +0000

    modified:   src/frontend/app.js

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -191,10 +191,12 @@ async function scanVisibleArea() {
             }
 
             const query = `[out:json][timeout:2];node["place"~"${placeFilter}"](${s},${w},${n},${e});out ${nodeLimit};`;
-            
+            const formData = new URLSearchParams();
+            formData.append("data", query);
+
             const overpassRes = await fetch(`https://overpass-api.de/api/interpreter`, {
                 method: 'POST',
-                body: query, 
+                body: formData,
                 signal: controller.signal
             });
             
